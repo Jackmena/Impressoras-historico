bb3dbf284b125d24c419b98e818ebe8625731b21
Jackson Mena <mena.jack72@gmail.com>
2024-11-17 02:05:41 -0300

    Conectando ao Railways

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -jar build\libs\java-rest-api-0.0.1-SNAPSHOT.jar
+
--- src/main/java/dio/projeto/java_rest_api/JavaRestApiApplication.java
+++ src/main/java/dio/projeto/java_rest_api/JavaRestApiApplication.java
@@ -3,6 +3,10 @@ package dio.projeto.java_rest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class JavaRestApiApplication {
 
